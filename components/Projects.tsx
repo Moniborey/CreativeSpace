9952255676a0af7ad00308dd53dd447e6d1fb0be
mam lyhua <mamlyhua@gmail.com>
2024-03-18 22:06:04 +0700

    please work bro hort nas please

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -56,7 +56,7 @@ export default function Projects() {
   );
   return (
     <Section isHscreen={false} id="projects" className="mt-14 md:mt-28 lg:mt-0">
-      <section className="flex flex-col sm:max-w-[700px] lg:max-w-max gap-10 md:gap-14 lg:gap-20 mx-auto px-5">
+      <section className="flex flex-col sm:max-w-[700px] lg:max-w-max gap-10 md:gap-14 lg:gap-20 mx-auto px-5 bg-blend-lighten">
         <h1
           className={` ${epilogue.className} text-4xl text-center text-gray-800 lg:text-5xl`}
         >
